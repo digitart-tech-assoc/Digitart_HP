68982b786a29ef1c32a0026b5aecafd6260e3d19
chrom9103 <kdkd21347.tm@gmail.com>
2026-02-27 20:48:29 +0900

    add: AboutPageとNewsPageコンポーネントを追加

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-slate-50 text-slate-900 font-sans selection:bg-emerald-200">
+      <Header />
+      <main className="flex-1 p-6 md:p-12 lg:p-24">
+      </main>
+      <Footer />
+    </div>
+  );
+}
