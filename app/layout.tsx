c1b311452702d75022274a555d6e7a16eadc7539
HyperMitra <legoomelet123@gmail.com>
2026-03-06 20:30:47 +0900

    Add header and footer layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,32 @@ export default function RootLayout({
           data-cf-beacon='{"token": "ec137f383473422da320b985a577919c"}'
         ></script>
       </body>
+
+      <header>
+  <div className="logo">
+    <img src="/logo.png" alt="Digitart Logo" />
+    <h1>Digitart</h1>
+  </div>
+
+  <nav>
+    <a href="/">ホーム</a>
+    <a href="/about">サークル紹介</a>
+    <a href="/activities">活動内容</a>
+    <a href="/work">作品</a>
+    <a href="/members">メンバー</a>
+    <a href="/contact">お問い合わせ</a>
+  </nav>
+</header>
+
+<main>{children}</main>
+
+<footer>
+  <p>© 2026 Digitart</p>
+  <p>青山学院大学 デジタルアートサークル</p>
+</footer>
     </html>
+
+
+
   );
 }
